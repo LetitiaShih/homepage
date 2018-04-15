b506c44086e21406091c0cc0beec3ecb0c49eab5
懿庭施 <letitias@AppledeMBP.domain.name>
2018-04-15 16:38:26 +0100

    Add Space Advanture

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, 'SpaceAdvanture_Web')))
+  // TODO: use the below homepage instead.
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile('index.html', { root: __dirname + "/SpaceAdvanture_Web/" } ))
+  .get('/SpaceAdvanture', (req, res) => res.sendFile('index.html', { root: __dirname + "/SpaceAdvanture_Web/" } ))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
