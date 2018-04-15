59dbfd1f47044d50fd6385cc181089b5ff60b0be
懿庭施 <letitias@AppledeMBP.domain.name>
2018-04-15 18:55:40 +0100

    Add home page

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'Homepage')))
   .use(express.static(path.join(__dirname, 'SpaceAdvanture_Web')))
   // TODO: use the below homepage instead.
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.sendFile('index.html', { root: __dirname + "/SpaceAdvanture_Web/" } ))
+  .get('/', (req, res) => res.sendFile('index.html', { root: __dirname + "/Homepage/" } ))
   .get('/SpaceAdvanture', (req, res) => res.sendFile('index.html', { root: __dirname + "/SpaceAdvanture_Web/" } ))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
